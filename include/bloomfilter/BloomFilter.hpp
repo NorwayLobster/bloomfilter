8b63618f229a84bee87586726e21e84c084efdfb
Cheng Wang <cheng.wang.801@gmail.com>
2020-11-23 17:39:02 +0000

    init repo

---
+++ include/bloomfilter/BloomFilter.hpp
@@ -0,0 +1,33 @@
+
+#ifndef __Bloom_Filter_Hpp__
+#define __Bloom_Filter_Hpp__
+
+#include <bitset>
+#include <string>
+//Define of BitVector and M
+#define len 1<<26
+#define M 5
+using bs=std::bitset<len>;
+
+class BloomFilter{
+public:
+	BloomFilter();
+	BloomFilter(const char* in_path);
+	~BloomFilter();
+
+	//Seralize bitset to file
+	void Dump(const char* out_path);
+	
+	//Add to Bloom Filter and set M bit to True
+	void Add(const ::std::string key);
+	
+	//if all of the M bit was set to True -> return True;if any of the M bit was set to False -> Return False
+	bool Test(const ::std::string key);
+
+	//Test first, add it if not in and return false, else return false
+	bool TestAndAdd(const ::std::string key);
+private:
+	 bs* _bs;
+};
+
+#endif
---
+++ src/BloomFilter.cc
@@ -0,0 +1,80 @@
+#include <string>
+#include <fstream>
+#include <stdint.h>
+#include "BloomFilter.hpp"
+#include "MurmurHash3.hpp"
+
+BloomFilter::BloomFilter()
+:_bs(NULL)
+{
+	this->_bs = new bs();
+	this->_bs->reset();
+}
+
+BloomFilter::BloomFilter(const char* in_path)
+:_bs(NULL)
+{
+	::std::ifstream in(in_path);
+	::std::string str((std::istreambuf_iterator<char>(in)),
+			std::istreambuf_iterator<char>());
+	this->_bs = new bs(str);
+	in.close();
+}
+
+BloomFilter::~BloomFilter()
+{
+	if(this->_bs)
+	{
+		delete _bs;	
+	}
+}
+
+void BloomFilter::Dump(const char* out_path)
+{
+	::std::ofstream out(out_path);
+	out<<this->_bs->to_string();
+	out.close();
+}
+
+void BloomFilter::Add(const ::std::string key)
+{
+	uint32_t seed = 0;
+	for(size_t i=0; i<M; i++)
+	{
+		uint32_t pos = 0;
+		MurmurHash3_x86_32(key.c_str(), key.length(), seed, &pos);
+		seed = pos;
+		pos %= LEN;
+		this->_bs->set(pos);
+	}
+}
+
+bool BloomFilter::Test(const ::std::string key)
+{
+	uint32_t seed = 0;
+	for(size_t i=0; i<M; i++)
+	{
+		uint32_t pos = 0;
+		MurmurHash3_x86_32(key.c_str(), key.length(), seed, &pos);
+		seed = pos;
+		pos %= LEN;
+		if(!this->_bs->test(pos))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+bool BloomFilter::TestAndAdd(const ::std::string key)
+{
+	if(this->Test(key))
+	{
+		return true;	
+	}
+	else
+	{
+		this->Add(key);
+		return false;
+	}
+}
